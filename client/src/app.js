f62e50097ecba43c23313a4d8999d118c8002ff7
Alice Loudon <alice_loudon@icloud.com>
2017-05-02 22:03:50 +0100

    Create SongContainer

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SongsContainer from './containers/songsContainer'
+import SongContainer from './containers/songContainer'
 
 window.onload = function(){
   ReactDOM.render(
-    <SongsContainer />,
+    <SongContainer />,
     document.getElementById('app')
   )
 }
---
+++ client/src/containers/SongContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class SongContainer extends React.Component {
+
+  render(){
+    return(
+      <div>
+        return song container
+      </div>
+    )
+  }
+
+}
+
+export default SongContainer
