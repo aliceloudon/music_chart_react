49edde76c7fc26500921050487203ad34eb09fbc
Alice Loudon <alice_loudon@icloud.com>
2017-05-02 21:59:27 +0100

    Initial commit

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SongsContainer from './containers/songsContainer'
+
+window.onload = function(){
+  ReactDOM.render(
+    <SongsContainer />,
+    document.getElementById('app')
+  )
+}
